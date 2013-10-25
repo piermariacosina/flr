05d85e83c4aa1e16f82d90ead63788709fcacd1b
Piermaria Cosina <piermaria@thedoersproject.com>
2013-10-25 07:32:54 +0200

    pressday newsletter

--- config.rb
+++ config.rb
@@ -39,6 +39,7 @@ page "/newsletters/summersale_registration2013.html", :layout => false
 page "/newsletters/pressday2013_1.html", :layout => false
 page "/newsletters/summersale_tea2013.html", :layout => false
 page "/stores.html", :layout => :html5
+page "/presskit.html", :layout => :html5
 
 page "/shop/list.html", :layout => :html5
 
