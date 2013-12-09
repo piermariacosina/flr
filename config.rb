d5f8939294343173ffb5affb21b1a4581013bdd3
Eleonora Giammello <eleonora.giammello@gmail.com>
2013-12-09 12:33:25 +0100

    modulo wowcracy per home

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ activate :deploy do |deploy|
 	deploy.user = "piermaria"
 	deploy.password = "Nonmelaricordo2"
 	deploy.path = "/home/piermaria/webapps/flavialarocca"
-	deploy.build_before = true
+	deploy.build_before = false
 	deploy.clean = true
 end
 
